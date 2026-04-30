47b159c59b226715ba2dba594518f970bff5532f
PustajicR <pustajic.r@gmail.com>
2026-04-30 10:54:53 +0200

    Changes done, reset button fr reset the scoreboard

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function addPoints(team, points) {
     scores[team] += points
     document.getElementById(team + 'points').textContent = scores[team]
 }
+function reset(){
+    homepoints = 0
+    guestpoints = 0
+    document.getElementById("homepoints").textContent = 0
+    document.getElementById("guestpoints").textContent = 0
+}
