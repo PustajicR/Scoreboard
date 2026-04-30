680c584a04b7e211ad94d516adc4047f665e1982
PustajicR <pustajic.r@gmail.com>
2026-04-30 11:01:41 +0200

    Fix the issues

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function addPoints(team, points) {
     scores[team] += points
     document.getElementById(team + 'points').textContent = scores[team]
 }
-function reset(){
-    homepoints = 0
-    guestpoints = 0
+function resetpoints() {
+    homescore = 0
+    guestscore = 0
     document.getElementById("homepoints").textContent = 0
     document.getElementById("guestpoints").textContent = 0
 }
